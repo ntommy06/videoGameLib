ce7f2d1113da3bde148c48d97aa1075f576c66f1
ntommy06 <tommyducnguyen@gmail.com>
2024-07-25 23:48:41 -0600

    Testing upstream issue

--- src/Message.tsx
+++ src/Message.tsx
@@ -3,7 +3,7 @@ function Message() {
   // JSX: JavasScript XML --> Gets compiled to javascript
   const name = 'Tommy';
   if(name)
-    return <h1>Hello {name}</h1>;
+    return <h1>Hello {name}, nice to meet you!</h1>;
   return <h1>Hello World!</h1>;
 }
 
